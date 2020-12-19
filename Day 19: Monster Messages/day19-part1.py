f576b7200ed1f1be9569d4acfa3f29706cb5b54e
Ryan-Kan <25820201+Ryan-Kan@users.noreply.github.com>
2020-12-19 22:57:39 +0800

    Upload Day 19 Solutions

---
+++ Day 19: Monster Messages/day19-part1.py
@@ -0,0 +1,72 @@
+"""
+day19-part1.py
+
+Created on 2020-12-19
+Updated on 2020-12-19
+
+Copyright © Ryan Kan
+"""
+
+# IMPORTS
+import re
+
+# INPUT
+with open("input-part1.txt", "r") as f:
+    sections = f.read().split("\n\n")
+    f.close()
+
+# Process rules
+lines = [line.strip() for line in sections[0].split("\n")]
+rules = {}
+for line in lines:
+    splitLine = line.split(": ")
+    rules[splitLine[0]] = splitLine[1]
+
+# Process messages
+messages = [line.strip() for line in sections[1].split("\n")[:-1]]
+
+
+# FUNCTIONS
+def generate_regex(rule_no):
+    """
+    Args:
+        rule_no (str)
+
+    Returns:
+        str:
+            The final RegEx expression.
+    """
+
+    # Get the rule that corresponds with the rule number
+    rule = rules[rule_no]
+
+    # If the rule matches the RegEx for a SINGLE character, just return the character
+    if re.fullmatch(r'"."', rule):
+        return rule[1]  # The second character of the rule will the SINGLE character
+    else:
+        # Split the rule into parts
+        rule_parts = rule.split(" | ")
+
+        # Process each part and generate their RegExes
+        resulting_regexes = []
+
+        for part in rule_parts:
+            rule_numbers = part.split(" ")
+            resulting_regexes.append("".join([generate_regex(rule_number) for rule_number in rule_numbers]))
+
+        # Form the final RegEx
+        return f"(?:{'|'.join(resulting_regexes)})"
+
+
+# COMPUTATION
+# Form the RegEx for rule number 0
+ruleZeroRegEx = generate_regex("0")
+
+# Check all the given messages to see if they match the RegEx pattern
+matchRuleZero = 0
+for message in messages:
+    if re.fullmatch(ruleZeroRegEx, message):
+        matchRuleZero += 1
+
+# OUTPUT
+print(matchRuleZero)
